2d4bec87cdb7a56686de475843fe5193ec345e8b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-13 19:44:50 +0300

    Added a home page structure

--- src/views/home.js
+++ src/views/home.js
@@ -5,7 +5,26 @@ export async function homePage(ctx) {
     // console.log(ctx);
     context = ctx;
     ctx.render(homeTemplate());
+    carousel();
+}
+
+var myIndex = 0;
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000);
 }
 
 const homeTemplate = () => html`
+<div class="slideshow-section">
+  <img class="mySlides" src="../images/home/1.jpg">
+  <img class="mySlides" src="img_ny.jpg">
+  <img class="mySlides" src="img_chicago.jpg">
+</div>
 `; 
