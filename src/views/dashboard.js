c7f2b559138ea51a492b0b79c12f0985b882d6f8
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-14 19:34:21 +0300

    Changed path to new assets

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -33,55 +33,42 @@ const initialTemplate = () => html`
     <div class="container">
 
         <a href="javascript:void(0)" class="logo">
-            <img src="/src/images/logo.png" alt="logo" width="364" height="58">
+            <img src="/src/images/logo.gif" alt="logo" width="364" height="58">
         </a>
 
         <div class="search-view" data-search-view>
-
             <div class="search-wrapper">
                 <input type="search" name="search" placeholder="Search city ..." 
                 autocomplete="off" class="search-field" data-search-field>
-
                 <span class="m-icon">search</span>
-
                 <button class="icon-btn leading-icon has-state" aria-label="close search" 
                 data-search-toggler>
                     <span class="m-icon">arrow_back</span>
                 </button>
-
-            </div>
+            </div> <!-- search-wrapper ends here -->
 
             <div class="search-result" data-search-result>
                 <ul class="view-list" data-search-list>
-
                     <li class="view-item">
                         <span class="m-icon">location_on</span>
-
                         <div>
-                        <p class="item-title">London</p>
-
-                        <p class="label-2 item-subtitle">State of London, GB</p>
+                            <p class="item-title">London</p>
+                            <p class="label-2 item-subtitle">State of London, GB</p>
                         </div>
-
                         <a href="javascript:void(0)" class="item-link has-state"
                         data-search-toggler></a>
                     </li>
-
                 </ul>
-            </div>
-
+            </div> <!-- search-result ends here -->
         </div> <!-- search-view ends here -->
 
 
-
-
         <div class="header-actions">
-
             <button class="icon-btn has-state" aria-label="open search" data-search-toggler>
                 <span class="m-icon icon">search</span>
             </button>
 
-            <!-- na a-to otgore (#/current-location) -->
+            <!-- below target = (#/current-location) -->
             <a href="javascript:void(0)" class="btn-primary has-state"data-current-location-btn>
                 <span class="m-icon">my_location</span>
 
