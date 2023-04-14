a3e81eb25cc4d31ef9b90ff9f083944e47c23227
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-14 18:38:59 +0300

    Styling & Structure updates

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -31,55 +31,62 @@ const noItemsTemplate = () => html`
 const initialTemplate = () => html`
 <div class="header">
     <div class="container">
+
         <a href="javascript:void(0)" class="logo">
-        <img src="/src/images/logo.png" alt="page-logo" width="364" height="58">
+            <img src="/src/images/logo.png" alt="logo" width="364" height="58">
         </a>
+
         <div class="search-view" data-search-view>
 
-        <div class="search-wrapper">
-            <input type="search" name="search" placeholder="Search city ..." autocomplete="off" class="search-field"
-            data-search-field>
+            <div class="search-wrapper">
+                <input type="search" name="search" placeholder="Search city ..." 
+                autocomplete="off" class="search-field" data-search-field>
 
-            <span class="m-icon">search</span>
+                <span class="m-icon">search</span>
 
-            <button class="icon-btn leading-icon has-state" aria-label="close search" data-search-toggler>
-            <span class="m-icon">arrow_back</span>
-            </button>
+                <button class="icon-btn leading-icon has-state" aria-label="close search" 
+                data-search-toggler>
+                    <span class="m-icon">arrow_back</span>
+                </button>
 
-        </div>
+            </div>
 
-        <div class="search-result" data-search-result>
-            <ul class="view-list" data-search-list>
+            <div class="search-result" data-search-result>
+                <ul class="view-list" data-search-list>
 
-            <li class="view-item">
-                <span class="m-icon">location_on</span>
+                    <li class="view-item">
+                        <span class="m-icon">location_on</span>
 
-                <div>
-                <p class="item-title">London</p>
+                        <div>
+                        <p class="item-title">London</p>
 
-                <p class="label-2 item-subtitle">State of London, GB</p>
-                </div>
+                        <p class="label-2 item-subtitle">State of London, GB</p>
+                        </div>
 
-                <a href="javascript:void(0)" data-search-toggler class="item-link has-state"></a>
-            </li>
+                        <a href="javascript:void(0)" class="item-link has-state"
+                        data-search-toggler></a>
+                    </li>
 
-            </ul>
-        </div>
+                </ul>
+            </div>
 
-        </div>
+        </div> <!-- search-view ends here -->
+
+
+
 
         <div class="header-actions">
 
-        <button class="icon-btn has-state" aria-label="open search" data-search-toggler>
-            <span class="m-icon icon">search</span>
-        </button>
+            <button class="icon-btn has-state" aria-label="open search" data-search-toggler>
+                <span class="m-icon icon">search</span>
+            </button>
 
-        <a href="javascript:void(0)" class="btn-primary has-state"data-current-location-btn>
             <!-- na a-to otgore (#/current-location) -->
-            <span class="m-icon">my_location</span>
+            <a href="javascript:void(0)" class="btn-primary has-state"data-current-location-btn>
+                <span class="m-icon">my_location</span>
 
-            <span class="span">Current Location</span>
-        </a>
+                <span class="span">Current Location</span>
+            </a>
         </div>
 
         <!-- TODO more -->
