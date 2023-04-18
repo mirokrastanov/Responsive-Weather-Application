6c12e6f4b5dee842626358bfa04324641509217b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-18 16:34:45 +0300

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -4,3 +4,23 @@ export const elements = [
     document.querySelector('header nav a:nth-of-type(3)'),
     document.querySelector('header nav a:nth-of-type(4)')
 ];
+
+export const daysFull = [
+    'Monday', 'Tuesday', 'Wednesday',
+    'Thursday', 'Friday', 'Saturday', 'Sunday'
+];
+
+export const daysShort = [
+    'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+];
+
+export const monthsFull = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+export const monthsShort = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
