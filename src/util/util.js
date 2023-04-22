c0b1da3e01d13336e9a65e82feeafe9801f91ae5
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-22 18:14:51 +0300

    Added a smart display updater for sub-10m

--- src/util/util.js
+++ src/util/util.js
@@ -124,7 +124,7 @@ export const weatherImgRoutesNIGHT = {
 
 export const timeParser = {
     hours24: (x = new Date()) => x.getHours() > 12 ? [x.getHours() % 12, 'PM'] : [x.getHours(), 'AM'],
-    min: (x = new Date()) => x.getMinutes(),
+    min: (x = new Date()) => x.getMinutes() < 10 ? `0${x.getMinutes()}` : x.getMinutes(),
     sec: (x = new Date()) => x.getSeconds(),
 };
 
