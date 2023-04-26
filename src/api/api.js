74f1e68ca4e4fc94015eff3a6dd76c55a9037e60
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-26 13:46:32 +0300

    Adding Geo & Reverse Geolocation API handlers

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,4 @@
+import { apiKey } from "../_private/api-private.js";
 import { dashboardElements, weatherCodes } from "../util/util.js";
 
 // latitude , longitude & timezone ==> added dynamically
@@ -14,3 +15,22 @@ export async function getWeather(lat, lon, timezone) {
     return data;
 }
 
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+export async function getGeolocation(searched) {
+    // city | city, country | city, state(US), country | 
+    let res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${searched}&limit=5&appid=${apiKey}`);
+    let data = await res; // returns JSON straight away
+
+    return data;
+}
+
+
+// http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
+export async function reverseGeolocation(lat, lon) {
+    // lat | lon
+    let res = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${apiKey}`);
+    let data = await res;
+
+    return data;
+}
