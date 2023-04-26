04aec70727314728b62f224691c942b52dc44621
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-26 14:12:30 +0300

    Rework of geocoding functionality

--- src/api/data-search.js
+++ src/api/data-search.js
@@ -2,43 +2,6 @@ import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import { arrayParser, dashboardElements } from '../util/util.js';
 
 
-// RE-IMPLEMENT THIS using open weather's api !!!
-// export async function reverseGeocoding(lat, lng) {
-//     let res = await fetch(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?f=json&featureTypes=&location=${lat},${lng}`);
-//     let data = await res.json();
-//     return data;
-// }
-
-let placeGlobal = null;
-export function getLocation(place) {
-    if (place == null) return;
-    delete place.utc_offset; // utc_offser is deprecated and throws errors
-    console.log(place);
-    placeGlobal = place;
-    const coords = getCoords(place);
-    console.log(coords);
-    window.place = place;
-    let info = [coords[0], coords[1], place.formatted_address, place];
-    let address = arrayParser.addressParser(place.formatted_address, place.name);
-    console.log(address);
-    address = address.split(', ');
-    if (address[address.length - 1] == 'USA') {
-        let stateUSA = place.formatted_address.split(', ').reverse().find(x => x.length == 2);
-        if (stateUSA != '' && stateUSA) {
-            address.splice(1, 0, stateUSA);
-        }
-    }
-    address = address.join(', ');
-    localStorage.setItem('lat', coords[0]);
-    localStorage.setItem('lng', coords[1]);
-    localStorage.setItem('address', address);
-    localStorage.setItem('full-address', place.formatted_address);
-    localStorage.setItem('place', JSON.stringify(place));
-    location.href = '/dashboard';
-}
-window.getLocation = getLocation; // allows the use of it inside the index.html !!!
-
-
 let searchTimeout = null;
 const searchTimeoutDuration = 500;
 
@@ -85,8 +48,3 @@ export function searchOnTyping(e) {
 
 }
 
-
-
-export function getCoords(place) {
-    return [place.geometry.location.lat(), place.geometry.location.lng()];
-}
