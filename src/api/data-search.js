fd24dcded9af1017861bd8ebdfeb6c76f34d7a3f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-26 16:55:18 +0300

    Geolocation data added

--- src/api/data-search.js
+++ src/api/data-search.js
@@ -1,19 +1,35 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import { arrayParser, dashboardElements, searchUtility } from '../util/util.js';
+import { getGeolocation } from './api.js';
+import { getParsedWeatherData, renderWeather } from './data-weather.js';
 
 
 let searchTimeout = null;
 const searchTimeoutDuration = 500;
 
 export function searchOnTyping(e) {
+    // runs on each input letter
     searchTimeout ?? clearTimeout(searchTimeout);
 
     if (!dashboardElements.searchField().value) {
         dashboardElements.searchField().classList.remove('searching');
+        dashboardElements.searchResult().classList.remove('active');
+        dashboardElements.searchResult().replaceChildren();
     } else {
         dashboardElements.searchField().classList.add('searching');
     }
 
     if (dashboardElements.searchField().value) {
+        searchTimeout = setTimeout(async () => {
+            try {
+                let geoL = await getGeolocation(dashboardElements.searchField().value);
+                if (!geoL) {
+                    console.log('No response!');
+                    return;
+                }
+                dashboardElements.searchField().classList.remove('searching');
+                dashboardElements.searchResult().classList.add('active');
+                dashboardElements.searchResult().replaceChildren();
 
 
 
