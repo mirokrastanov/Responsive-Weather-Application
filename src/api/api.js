c56fc71f1546b628f3a8cd7100cecbb7f3cc801b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-29 14:52:48 +0300

    AQI API added and tested

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,24 @@
+// import axios from "axios";
 import { apiKey } from "../_private/api-private.js";
 import { dashboardElements, weatherCodes } from "../util/util.js";
 
 // latitude , longitude & timezone ==> added dynamically
 export async function getWeather(lat, lon, timezone) {
     let res = await axios.get('https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,weathercode,pressure_msl,cloudcover,visibility,windspeed_10m,winddirection_10m,is_day&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,precipitation_probability_max&current_weather=true&windspeed_unit=ms&timeformat=unixtime', {
+        params: {
+            latitude: lat,
+            longitude: lon,
+            timezone,
+        }
+    });
+    let data = await res.data;
+
+    return data;
+}
+
+// latitude , longitude & timezone ==> added dynamically
+export async function getAQI(lat, lon, timezone) {
+    let res = await axios.get('https://air-quality-api.open-meteo.com/v1/air-quality?hourly=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone,dust,european_aqi&timeformat=unixtime', {
         params: {
             latitude: lat,
             longitude: lon,
