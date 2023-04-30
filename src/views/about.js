b2f587655b44efdc0f4a8405b8d60c72a6960238
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-30 14:13:13 +0300

    Notes for next day of development

--- src/views/about.js
+++ src/views/about.js
@@ -8,11 +8,12 @@ export async function aboutPage(ctx) {
 
 const aboutTemplate = () => html`
 <div>TEST - ABOUT</div>
-<span class="error-overlay">Read ALL notes from the last month 
-    to fill the Credits PROPERLY and to not miss something!!!
-    --- _templates/about.md --- read that for the img credits</span>
-`; 
+<span class="error-overlay">
+    <p>Read ALL notes from the last month to fill the Credits PROPERLY!!!</p>
+    <p>Make sure that you don't miss something!!!</p>
+    <p>_templates/about.md - read that for the img credits</p>
+</span>
+`;
 
 
 //  <div><a href="https://github.com/mirokrastanov" target="_blank">Author</a></div>
- 
