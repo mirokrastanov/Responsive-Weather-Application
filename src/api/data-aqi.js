6b1da988f1728f571d25ad96bc345dececc6336b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-01 13:46:18 +0300

    Implemented units into each hourly element

--- src/api/data-aqi.js
+++ src/api/data-aqi.js
@@ -7,11 +7,12 @@ import {
 } from "../util/util.js";
 import { dashboardHourlyCardLower, dashboardHourlyCardUpper, dynamicHourlyTemplate } from '../views/dashboard.js';
 import { getAQI, getTimeZoneWeather, getWeather, reverseGeolocation } from "./api.js";
-import { getCurrentTimeZone, returnDayLONG, returnHour } from './data-weather.js';
+import { getCurrentTimeZone, returnDayLONG, returnHour, setValue } from './data-weather.js';
 
 
 function parseAQIData(data) {
     let { hourly } = data;
+    // console.log(data);
     return hourly.time.map((time, index) => {
         return {
             timestamp: time * 1000,
@@ -19,14 +20,14 @@ function parseAQIData(data) {
             dayLong: returnDayLONG().format(time * 1000),
             date: new Date(time * 1000).getDate(),
             monthShort: monthsShort[new Date(time * 1000).getMonth()],
-            carbon_monoxide: hourly.carbon_monoxide[index],
-            dust: hourly.dust[index],
-            european_aqi: hourly.european_aqi[index],
-            nitrogen_dioxide: hourly.nitrogen_dioxide[index],
-            ozone: hourly.ozone[index],
-            pm2_5: hourly.pm2_5[index],
-            pm10: hourly.pm10[index],
-            sulphur_dioxide: hourly.sulphur_dioxide[index],
+            carbon_monoxide: [hourly.carbon_monoxide[index], "μg/m³"],
+            dust: [hourly.dust[index], "μg/m³"],
+            european_aqi: [hourly.european_aqi[index], "EAQI"],
+            nitrogen_dioxide: [hourly.nitrogen_dioxide[index], "μg/m³"],
+            ozone: [hourly.ozone[index], "μg/m³"],
+            pm2_5: [hourly.pm2_5[index], "μg/m³"],
+            pm10: [hourly.pm10[index], "μg/m³"],
+            sulphur_dioxide: [hourly.sulphur_dioxide[index], "μg/m³"],
         }
     }).filter(({ timestamp }) => timestamp >= new Date().getTime());
     // filters only the hours from the current hour to after 6 days
@@ -36,7 +37,6 @@ function parseAQIData(data) {
 // i 2ta - getParsedAQI i getParsedWeather da se invoke-vat i na dvata page-a, samo render-a, da
 // si e konkreten za teky6tia page
 export async function getParsedAQIData(coords) {
-    // let testRaw = await getAQI(42.7, 23.32, getCurrentTimeZone());
     let data = await getAQI(coords[0], coords[1], getCurrentTimeZone());
     let parsedData = parseAQIData(data);
     let extraData = await getTimeZoneWeather(coords[0], coords[1]);
@@ -47,16 +47,15 @@ export async function getParsedAQIData(coords) {
         units: data.hourly_units,
     };
     result.timeZoneGMTdiff = locationTimeZone;
-    // if time/clock is needed - check 298+ lines from data-weather
     return result;
 }
 
 
 export function renderDashboardAQI(page, data) {
-    // console.log(data);
+    console.log(data);
     const { hourly, units } = data;
     if (page == 'dashboard') {
-
+        // setValue(dashboardElements.highAQIstate(), )
 
     } else if (page == 'air-quality') {
 
