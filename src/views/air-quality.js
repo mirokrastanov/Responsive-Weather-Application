7c1b74140a1937df12ea7274dd50ac2ee1f3e100
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-02 14:41:19 +0300

    Updated dynamic AQI hourly section

--- src/views/air-quality.js
+++ src/views/air-quality.js
@@ -36,7 +36,7 @@ export async function airQualityPage(ctx) {
         removeErrorOverlay();
         console.log(weatherInfo);
         console.log(aqiInfo);
-        
+        aqiElements.btnsALL().forEach(x => x.addEventListener('click', onHourlyBtnClick));
     } catch (error) {
         let message = 'Error getting Air Quality data!';
         console.log('Error details: ', { ...error, 'stack': error.stack });
@@ -45,6 +45,96 @@ export async function airQualityPage(ctx) {
         applyBlur(aqiElements.aqiWrapper());
     }
 }
+
+
+function onHourlyBtnClick(e) {
+    e.preventDefault();
+    let btn = e.target;
+    hideAqiBoxes(btn);
+    generateRow(currentAQIinfo);
+
+}
+
+
+function hideAqiBoxes(btn) {
+    aqiElements.btnsALL().forEach(x => {
+        if (x != btn) x.parentElement.parentElement.style.display = 'none';
+    });
+
+}
+
+function generateRow(item = {}) {
+    let div = document.createElement('div');
+    div.classList.add('aqi-row-all');
+
+    render(aqiBoxRowTemplate(item), div);
+    aqiElements.aqiFlexGrid().appendChild(div);
+
+}
+
+const aqiBoxRowTemplate = (item = {}) => html`
+    <div class="aqi-cell">
+        <p class="aqi-title">Apr 17</p>
+        <p class="aqi-content">3 PM</p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Particles less than 2.5 µm (PM2.5)">
+            PM<sub>2.5</sub></p>
+        <p class="aqi-content" title="Particles less than 2.5 µm (PM2.5)">
+            23 <sub>μg/m³</sub></p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Particles less than 10 µm (PM10)">
+            PM<sub>10</sub></p>
+        <p class="aqi-content" title="Particles less than 10 µm (PM10)">
+            16 <sub>μg/m³</sub></p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Nitrogen dioxide (NO2)">
+            NO<sub>2</sub></p>
+        <p class="aqi-content" title="Nitrogen dioxide (NO2)">
+            90 <sub>μg/m³</sub></p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Ozone (O3)">
+            O<sub>3</sub></p>
+        <p class="aqi-content" title="Ozone (O3)">
+            41 <sub>μg/m³</sub></p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Sulphur dioxide (SO2)">
+            SO<sub>2</sub></p>
+        <p class="aqi-content" title="Sulphur dioxide (SO2)">
+            75 <sub>μg/m³</sub></p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Carbon Monoxide (10m above ground)">
+            CO</p>
+        <p class="aqi-content" title="Carbon Monoxide (10m above ground)">
+            24 <sub>μg/m³</sub></p>
+    </div>
+    <div class="aqi-cell">
+        <p class="aqi-title" title="Dust particles (10m above ground)">
+            Dust</p>
+        <p class="aqi-content" title="Dust particles (10m above ground)">
+            61 <sub>μg/m³</sub></p>
+    </div>
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
